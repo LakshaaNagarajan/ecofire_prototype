cb2b8610e44a7fb3e01a65b85fd433c8489f5adb
Dhruv Sunil Parmar <parmar.dhru@northeastern.edu>
2025-02-09 19:08:30 -0500

    initial commit, added clerk auth and shadcn

---
+++ components/dashboard/app-sidebar.tsx
@@ -0,0 +1,92 @@
+import { Calendar, Home, Inbox, Search, Settings, Download, Dog } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Quick Guide",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Jobs",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Backstage",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Team",
+    url: "#",
+    icon: Settings,
+  },
+  {
+    title: "Jija",
+    url: "#",
+    icon: Dog,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>EcoF:re</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton size={"lg"} asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <SidebarMenuButton size={"lg"} asChild>
+                    <a href="#">
+                    <Settings />
+                    <span>Settings</span>
+                    </a>
+                </SidebarMenuButton>
+                <SidebarMenuButton size={"lg"} asChild>
+                    <a href="#">
+                    <Download />
+                    <span>Business Success Plan
+                    </span>
+                    </a>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}

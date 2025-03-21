5b220ecbfc543b538cb30a03f8bd6f908d55e308
Shrikant Kshirsagar <shrikantkshirsagar29@gmail.com>
2025-03-21 19:53:23 -0400

    Adding padding b/w messages & input fields

--- app/dashboard/jija/page.tsx
+++ app/dashboard/jija/page.tsx
@@ -23,7 +23,7 @@ export default function Chat() {
   });
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div className="flex flex-col w-full max-w-md pb-48 py-24 mx-auto stretch">
       {messages.map(m => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === 'user' ? 'User: ' : 'AI: '}

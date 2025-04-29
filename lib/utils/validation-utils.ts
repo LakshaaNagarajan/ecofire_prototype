8135456cd0b2fb89e02337391a6b30f07f369601
tobharti <tobharti@yahoo.com>
2025-04-29 08:18:56 -0700

    Allow special characters (#154)

--- lib/utils/validation-utils.ts
+++ lib/utils/validation-utils.ts
@@ -13,9 +13,10 @@ export async function validateString(str_name: string) {
   if (str_name.length > 50) {
     throw new ValidationError('field must be at most 50 characters long', 400);
   }
-  if (!/^[a-zA-Z0-9 ]+$/.test(str_name)) {
-    throw new ValidationError('field can only contain alphanumeric characters and spaces', 400);
+  if (!/^[a-zA-Z0-9 '"’.,!?@#&()\-_:;\\/[\]{}<>|`~$%^*=+]+$/.test(str_name)) { //Allow letters, numbers, spaces, quotes, apostrophes, and common special characters:
+    throw new ValidationError('field contains invalid characters', 400);
   }
+  
   if (str_name.trim() === '') {
     throw new ValidationError('field cannot be empty',  400);
   }
